0c54be404a4f7d014ad5f1d2eac29c142d0c8db3
Pedro Bonfilio <pedroblimaa@gmail.com>
2021-06-21 08:19:04 -0300

    Adicionado barra de navegação e começo da primeira página

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav">
+    <h2 class="nav-title"> <router-link to="/"> Pedro Bonfilio</router-link></h2>
+    <div class="nav-options">
+      <router-link to="/">About</router-link>
+      <router-link to="/works">Works</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import './NavBar.css'
+
+export default {
+  name: "NavBar",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
---
+++ src/views/About/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <div class="block main-block">
+      <div class="main-card">
+        My name is Pedro Bonfilio Lima, I'm a Full-Stack developer, currently attending
+        control and automation course . Please have a seat, scroll down, and I'll get you
+        a coffee.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import './About.css'
+
+export default {
+  name: 'About',
+}
+</script>
